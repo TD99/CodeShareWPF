c1c1985ad9056a7a006bc9b9edff1e8d01b73371
Tim Dürr <tim@duerrs.ch>
2023-06-08 10:28:48 +0200

    Added User Page

---
+++ CodeShare/MVVM/View/UserPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CodeShare.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for UserPage.xaml
+    /// </summary>
+    public partial class UserPage : Page
+    {
+        public UserPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
