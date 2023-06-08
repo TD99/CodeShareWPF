e0fbc77eb40818f59e53399abebb4e84b96cac47
Tim Dürr <tim@duerrs.ch>
2023-06-08 09:31:15 +0200

    Add project files.

---
+++ CodeShare/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace CodeShare
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ CodeShare/ToolbarWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace CodeShare
+{
+    /// <summary>
+    /// Interaction logic for ToolbarWindow.xaml
+    /// </summary>
+    public partial class ToolbarWindow : Window
+    {
+        public ToolbarWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
