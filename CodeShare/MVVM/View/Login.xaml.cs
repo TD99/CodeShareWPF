8d8b1fe8eb1202c7a97233a86a5ffa3dcb168b96
Jakob <jjo129634@iet-gibb.ch>
2023-06-15 10:56:21 +0200

    Login/register

---
+++ CodeShare/MVVM/View/Login.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CodeShare.MVVM.View
+{
+    /// <summary>
+    /// Interaktionslogik für Page1.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        Register register = new Register();
+        /*private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxEmail.Text.Length == 0)
+            {
+                errormessage.Text = "Enter an email.";
+                textBoxEmail.Focus();
+            }
+            else if (!Regex.IsMatch(textBoxEmail.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                errormessage.Text = "Enter a valid email.";
+                textBoxEmail.Select(0, textBoxEmail.Text.Length);
+                textBoxEmail.Focus();
+            }
+            else
+            {
+                string email = textBoxEmail.Text;
+                string password = passwordBox1.Password;
+                SqlConnection con = new SqlConnection("Data Source=TESTPURU;Initial Catalog=Data;User ID=sa;Password=wintellect");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from Registration where Email='" + email + "'  and password='" + password + "'", con);
+                cmd.CommandType = CommandType.Text;
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.SelectCommand = cmd;
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                if (dataSet.Tables[0].Rows.Count > 0)
+                {
+                    string username = dataSet.Tables[0].Rows[0]["Name"].ToString() + " " + dataSet.Tables[0].Rows[0]["GitUserName"].ToString();
+                }
+                else
+                {
+                    errormessage.Text = "Sorry! Please enter existing emailid/password.";
+                }
+                con.Close();
+            }
+        }*/
+        private void buttonRegister_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Register());
+        }
+    }
+}
--- CodeShare/MVVM/View/UserPage.xaml.cs
+++ CodeShare/MVVM/View/UserPage.xaml.cs
@@ -1,15 +1,28 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Navigation;
 
 namespace CodeShare.MVVM.View
 {
     /// <summary>
-    /// Interaction logic for UserPage.xaml
+    /// Interaktionslogik für UserPage.xaml
     /// </summary>
     public partial class UserPage : Page
     {
         public UserPage()
         {
             InitializeComponent();
+
+            Navigate(new Register());
+
+        }
+        public void Navigate(Page page)
+        {
+            UserFrame.Navigate(page);
+        }
+
+        private void UserFrame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            UserFrame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
         }
     }
 }
